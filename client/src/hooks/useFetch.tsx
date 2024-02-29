44a8112324c569938bcb9bace2e0088b9bb79da9
choudhari1 <57314380+choudhari1@users.noreply.github.com>
2024-02-29 09:01:55 +0000

    "store setup"

--- client/src/hooks/useFetch.tsx
+++ client/src/hooks/useFetch.tsx
@@ -1,4 +1,6 @@
 import React,{useState,useEffect} from "react";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../redux/strore";
 
 /**
  * @description This is Custome Hoook which use to fetch the data from the get API
@@ -29,3 +31,6 @@ export const useFecth=(props:UseFecthInterface)=>{
 
     return [loading , data ,error];
 }
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
--- client/src/redux/strore.ts
+++ client/src/redux/strore.ts
@@ -1,1 +1,14 @@
 import React from "react";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { sampleSlice } from "../slice/sampleSlice";
+
+// Add all your reducers under the cReducers function 
+const cReducers = combineReducers({
+    samplevalue: sampleSlice.reducer
+})
+export const store = configureStore({
+    reducer: cReducers
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
